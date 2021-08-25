6cd6c37cd98a939b487bd0eb3acc1bc25091dad9
Dimk00z <dimk00z@gmail.com>
2021-08-25 18:27:19 +0300

    update schema

--- schema_design/db_schema.sql
+++ schema_design/db_schema.sql
@@ -7,46 +7,46 @@ CREATE SCHEMA IF NOT EXISTS content;
 -- Create films table
 CREATE TABLE IF NOT EXISTS content.film_work
 (
-    id uuid,
-    title character varying(250) NOT NULL,
-    description text,
+    id            uuid,
+    title         character varying(250) NOT NULL,
+    description   text,
     creation_date date,
-    certificate text,
-    file_path character varying(300),
-    rating numeric,
-    type character varying(30) NOT NULL,
-    created_at timestamp with time zone,
-    updated_at timestamp with time zone,
+    certificate   text,
+    file_path     character varying(300),
+    rating        numeric,
+    type          character varying(30)  NOT NULL,
+    created_at    timestamp with time zone,
+    updated_at    timestamp with time zone,
     PRIMARY KEY (id)
 );
 
 -- Create genres table
 CREATE TABLE IF NOT EXISTS content.genre
 (
-    id uuid,
-    name character varying(100) NOT NULL,
+    id          uuid,
+    name        character varying(100) NOT NULL,
     description text,
-    created_at timestamp with time zone,
-    updated_at timestamp with time zone,
+    created_at  timestamp with time zone,
+    updated_at  timestamp with time zone,
     PRIMARY KEY (id)
 );
 -- Create table for actors
 CREATE TABLE IF NOT EXISTS content.person
 (
-    id uuid,
-    full_name character varying(200) NOT NULL,
+    id         uuid,
+    full_name  character varying(200) NOT NULL,
     birth_date date,
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
     PRIMARY KEY (id)
 );
--- Create table for many-to-many relatiomship for films and genres
+-- Create table for many-to-many relationship for films and genres
 CREATE TABLE IF NOT EXISTS content.genre_film_work
 (
-    id uuid,
+    id           uuid,
     film_work_id uuid NOT NULL,
-    genre_id uuid NOT NULL,
-    created_at timestamp with time zone,
+    genre_id     uuid NOT NULL,
+    created_at   timestamp with time zone,
     PRIMARY KEY (id),
     UNIQUE (film_work_id, genre_id),
     CONSTRAINT film_work_id FOREIGN KEY (film_work_id)
@@ -59,14 +59,14 @@ CREATE TABLE IF NOT EXISTS content.genre_film_work
         ON DELETE NO ACTION
 );
 
--- Create table for many-to-many relatiomship for films and actors
+-- Create table for many-to-many relationship for films and actors
 CREATE TABLE IF NOT EXISTS content.person_film_work
 (
-    id uuid,
+    id           uuid,
     film_work_id uuid NOT NULL,
-    person_id uuid NOT NULL,
-    role character varying(150),
-    created_at timestamp with time zone,
+    person_id    uuid NOT NULL,
+    role         character varying(150),
+    created_at   timestamp with time zone,
     PRIMARY KEY (id),
     UNIQUE (film_work_id, person_id),
     CONSTRAINT "FK_film_work_id" FOREIGN KEY (film_work_id)

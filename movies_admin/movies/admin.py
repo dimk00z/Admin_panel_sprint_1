ab9300ea18cfd6bcbc8f72b741ca9ddbac2f0b69
Dimk00z <dimk00z@gmail.com>
2021-08-27 16:22:23 +0300

    register films

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Genre, FilmWork, PersonFilmWork
 # Register your models here.
+
+
+class PersonRoleInline(admin.TabularInline):
+    model = PersonFilmWork
+    extra = 0
+
+
+@admin.register(FilmWork)
+class FilmWorkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'creation_date', 'rating')
+    fields = (
+        'title', 'type', 'description', 'creation_date', 'certificate',
+        'file_path', 'rating',
+    )
+    inlines = [
+        PersonRoleInline
+    ]

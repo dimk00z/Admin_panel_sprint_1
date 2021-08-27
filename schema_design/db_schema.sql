e43b6318f3b0e1f557acb7dcd539abf2e502c4ac
Dimk00z <dimk00z@gmail.com>
2021-08-27 14:37:30 +0300

    add foreign keys

--- schema_design/db_schema.sql
+++ schema_design/db_schema.sql
@@ -4,7 +4,7 @@
 -- Create schema for current content
 CREATE SCHEMA IF NOT EXISTS content;
 
--- Create films table
+-- Create tables
 CREATE TABLE IF NOT EXISTS content.film_work
 (
     id            uuid,
@@ -20,7 +20,6 @@ CREATE TABLE IF NOT EXISTS content.film_work
     PRIMARY KEY (id)
 );
 
--- Create genres table
 CREATE TABLE IF NOT EXISTS content.genre
 (
     id          uuid,
@@ -30,7 +29,7 @@ CREATE TABLE IF NOT EXISTS content.genre
     updated_at  timestamp with time zone,
     PRIMARY KEY (id)
 );
--- Create table for actors
+
 CREATE TABLE IF NOT EXISTS content.person
 (
     id         uuid,
@@ -40,27 +39,37 @@ CREATE TABLE IF NOT EXISTS content.person
     updated_at timestamp with time zone,
     PRIMARY KEY (id)
 );
--- Create table for many-to-many relationship for films and genres
+
 CREATE TABLE IF NOT EXISTS content.genre_film_work
 (
-    id           uuid,
+    id uuid,
     film_work_id uuid NOT NULL,
-    genre_id     uuid NOT NULL,
-    created_at   timestamp with time zone,
+    genre_id uuid NOT NULL,
+    created_at timestamp without time zone,
     PRIMARY KEY (id),
-    UNIQUE (film_work_id, genre_id)
+    UNIQUE (film_work_id, genre_id),
+    CONSTRAINT "Filmwork" FOREIGN KEY (film_work_id)
+        REFERENCES content.film_work (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT "Genre" FOREIGN KEY (genre_id)
+        REFERENCES content.genre (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
--- Create table for many-to-many relationship for films and actors
 CREATE TABLE IF NOT EXISTS content.person_film_work
 (
-    id           uuid,
+    id uuid,
+    role character varying(50),
     film_work_id uuid NOT NULL,
-    person_id    uuid NOT NULL,
-    role         character varying(150),
-    created_at   timestamp with time zone,
+	person_id uuid NOT NULL,
+    created_at timestamp with time zone,
     PRIMARY KEY (id),
-    UNIQUE (film_work_id, person_id, role)
+    CONSTRAINT "Filmwork" FOREIGN KEY (film_work_id)
+        REFERENCES content.film_work (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 -- Create indexes
